71acc0782ce4ca8d02ecae723341dfcc231029df
Baabah <mail@peterton.nl>
2015-05-03 10:34:40 +0200

    Added location apiclient

---
+++ src/LocationManagement/IpinfodbApiClient.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Peter
+ * Date: 3-5-2015
+ * Time: 10:32
+ */
+
+namespace LocationManagement;
+
+
+use Interfaces\ApiCient;
+
+class IpinfodbApiClient implements ApiCient {
+
+    public function getData() {
+
+    }
+
+} 
---
+++ src/LocationManagement/LocationManager.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace LocationManagement;
+
+
+class LocationManager {
+
+} 
