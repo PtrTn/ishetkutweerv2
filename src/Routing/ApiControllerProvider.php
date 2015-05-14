b6a3ed1d50cfde29b254437f2752d816f28ea7a0
Baabah <mail@peterton.nl>
2015-05-14 19:55:47 +0200

    Adjusted API endpoints

--- src/Routing/ApiControllerProvider.php
+++ src/Routing/ApiControllerProvider.php
@@ -30,9 +30,17 @@ class ApiControllerProvider implements ControllerProviderInterface
         );
 
         $controllers->get(
+            '/',
+            function () use ($app) {
+                return $app['twig']->render('api.twig');
+            }
+        );
+
+        $controllers->get(
             '/weather',
             function () use ($app) {
-                return $app->json($app['api_data_provider']->getWeatherData());
+                // Todo use IP-based lat/lon
+                return $app->json($app['api_data_provider']->getWeatherDataByLatLon(51.44126129, 3.59591007));
             }
         );
 
