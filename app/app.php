23cfc040eeadab703dc760fead0a1b3110119056
Peter Ton <peter@senturo.nl>
2016-01-14 17:27:33 +0100

    Wired location and data providing up

--- app/app.php
+++ app/app.php
@@ -1,18 +1,17 @@
 <?php
 
 date_default_timezone_set('Europe/Amsterdam');
-
 // Load application
 require('bootstrap.php');
 $app = new Silex\Application();
 $app['debug'] = true;
 
 $app->register(new Silex\Provider\TwigServiceProvider(), array(
-        'twig.path' => __DIR__ . '/resources/views',
-    ));
+    'twig.path' => __DIR__ . '/resources/views',
+));
 
 // Load config file
-$app->register(new DerAlex\Silex\YamlConfigServiceProvider( __DIR__ . '/config/config.yaml'));
+$app->register(new DerAlex\Silex\YamlConfigServiceProvider(__DIR__ . '/config/config.yaml'));
 
 // Get database config based on env
 $databaseConfig = $app['config']['prod']['database'];
@@ -27,26 +26,63 @@ $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
         'user' => $databaseConfig['user'],
         'password' => $databaseConfig['password'],
         'host' => $databaseConfig['host'],
-        'driver'   => $databaseConfig['driver']
+        'driver' => $databaseConfig['driver']
     ),
 ));
+$dis = new JeroenDesloovere\Distance\Distance;
 
 $date = date('md');
-$result = $app['db']->fetchAll("SELECT * FROM weatherdata WHERE date LIKE ? ORDER BY date", ['%' . $date]);
-var_dump($date);
-var_dump($result);
+$result = $app['db']->fetchAll("SELECT * FROM weatherdata WHERE date LIKE ? ORDER BY date", array('%' . $date));
+//var_dump($date);
+//var_dump($result);
 
 // Set homepage
 $app->get('/', function () use ($app) {
-        return $app['twig']->render('home.twig');
-    });
+    return $app['twig']->render('home.twig');
+});
 
-// Locatie omzetten naar weerstation
 
-// Gegevens van weerstation ophalen
+$app['locator'] = function () use ($app) {
+    return new \Location\Locator();
+};
+$app['distanceCalc'] = function () {
+    return new JeroenDesloovere\Distance\Distance;
+};
+$app['stationFactory'] = function () use ($app) {
+    return new \Location\StationFactory();
+};
+$app['stationFinder'] = function () use ($app) {
+    return new \Location\StationFinder($app['stationFactory'], $app['distanceCalc']);
+};
+$app['presentDataProvider'] = function () use ($app) {
+    return new \DataProviding\PresentDataProvider();
+};
+$app['historicDataProvider'] = function () use ($app) {
+    return new \DataProviding\HistoricDataProvider($app['db']);
+};
 
-// Gegevens verwerken (rating geven etc)
+// Get IP using connection details
+$ip = $_SERVER['REMOTE_ADDR'];
+if ($app['debug'] === true) {
+    $ip = '213.34.236.130';
+}
 
-// Gegevens formatten voor weergaven
+// Get location based on IP
+$location = $app['locator']->getLocation($ip);
+
+// Get station based on location
+$station = $app['stationFinder']->findStation($location);
+
+// Get data based on station
+$presentData = $app['presentDataProvider']->getDataByStation($station);
+$historicData = $app['historicDataProvider']->getDataByStation($station);
+
+var_dump($station);
+var_dump($presentData);
+var_dump($historicData);
+
+// TODO Gegevens verwerken (rating geven etc)
+
+// TODO Gegevens formatten voor weergaven
 
 return $app;
