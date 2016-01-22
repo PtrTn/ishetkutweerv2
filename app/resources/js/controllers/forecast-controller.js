1ef1c436a67b266326fd64d9b9ea2da2367bf4b2
Peter Ton <peter@senturo.nl>
2016-01-22 14:42:39 +0100

    Changed summary/header consturction

--- app/resources/js/controllers/forecast-controller.js
+++ app/resources/js/controllers/forecast-controller.js
@@ -1,9 +1,14 @@
 forecastCtrl = ['$scope', function ($scope) {
 
     $scope.more = false;
+    $scope.longMsg = false;
 
     $scope.toggleMore = function () {
         $scope.more = !$scope.more;
-    }
+    };
+
+    $scope.toggleSummaryMsg = function () {
+        $scope.longMsg = !$scope.longMsg;
+    };
 }];
 app.controller('forecastCtrl', forecastCtrl);
--- app/resources/js/controllers/summary-controller.js
+++
@@ -1,9 +0,0 @@
-summaryCtrl = ['$scope', function ($scope) {
-
-    $scope.longMsg = false;
-
-    $scope.toggleSummaryMsg = function () {
-        $scope.longMsg = !$scope.longMsg;
-    }
-}];
-app.controller('summaryCtrl', summaryCtrl);
