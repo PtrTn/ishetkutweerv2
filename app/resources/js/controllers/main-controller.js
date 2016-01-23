a0408d4f9b715dd150ac567c4d4e13ca34ef15bc
Peter Ton <peter@senturo.nl>
2016-01-23 18:58:04 +0100

    Removed console log

--- app/resources/js/controllers/main-controller.js
+++ app/resources/js/controllers/main-controller.js
@@ -1,9 +1,9 @@
 mainCtrl = ['$scope', function ($scope) {
 
     $scope.toggleMenu = function () {
-        console.log('toggle');
         $('.overlay').toggle();
         $('.menu').toggle();
     };
+
 }];
 app.controller('mainCtrl', mainCtrl);
