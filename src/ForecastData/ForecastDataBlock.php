851c944b2aaca9e0497dd0f99df6b94f82ceef8b
Peter Ton <peter@senturo.nl>
2016-02-08 12:59:07 +0100

    Created dateformatter

--- src/ForecastData/ForecastDataBlock.php
+++ src/ForecastData/ForecastDataBlock.php
@@ -3,6 +3,7 @@
 namespace ForecastData;
 
 use Helpers\BeaufortCalculator;
+use Helpers\DateFormatter;
 use Interfaces\DataBlock;
 
 class ForecastDataBlock implements DataBlock
@@ -64,66 +65,16 @@ class ForecastDataBlock implements DataBlock
 
     public function getFormattedDate()
     {
-        $month = $this->getMonth($this->date->format('n'));
+        $monthOfYear = $this->date->format('n');
+        $monthName = DateFormatter::getMonthName($monthOfYear);
         $day = $this->date->format('j');
-        return $day . ' ' . $month;
+        return $day . ' ' . $monthName;
     }
 
-    // TODO move this to helper
-    private function getMonth($month)
-    {
-        switch ($month) {
-            case 1:
-                return 'Januari';
-            case 2:
-                return 'Februari';
-            case 3:
-                return 'Maart';
-            case 4:
-                return 'April';
-            case 5:
-                return 'Mei';
-            case 6:
-                return 'Juni';
-            case 7:
-                return 'Juli';
-            case 8:
-                return 'Augustus';
-            case 9:
-                return 'Septebmer';
-            case 10:
-                return 'Oktober';
-            case 11:
-                return 'November';
-            case 12:
-                return 'December';
-            default:
-                return false;
-        }
-    }
-
-    // TODO move this to helper
     public function getDayName()
     {
-        $dayofWeek = $this->date->format('N');
-        switch ($dayofWeek) {
-            case 1:
-                return 'Maandag';
-            case 2:
-                return 'Dinsdag';
-            case 3:
-                return 'Woensdag';
-            case 4:
-                return 'Donderdag';
-            case 5:
-                return 'Vrijdag';
-            case 6:
-                return 'Zaterdag';
-            case 7:
-                return 'Zondag';
-            default:
-                return false;
-        }
+        $dayOfWeek = $this->date->format('N');
+        return DateFormatter::getDayName($dayOfWeek);
     }
 }
  
